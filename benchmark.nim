14cc4ec2e63416244de635e81439eacb36fd6132
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-08-08 17:33:04 +0800

    update benchmark

--- benchmark.nim
+++ benchmark.nim
@@ -5,6 +5,7 @@ import checksums/sha2
 import hashlib//mhash/sha512
 import hashlib/rhash/sha512
 import hashlib/sph/sha512
+import hashlib/misc/crypto
 import nimcrypto/sha2
 
 template timeit(body: untyped) =
@@ -20,25 +21,29 @@ proc sha512_checksums(s: string): string =
   hasher.digest.`$`
 
 proc sha512_rhash(s: string): string =
-  ## ~2.5s/Mhash
+  ## ~1.1s/Mhash
   count[RHASH_SHA512](s).`$`
 
 proc sha512_mhash(s: string): string =
-  ## ~2.8s/Mhash
+  ## ~1.2s/Mhash
   count[MHASH_SHA512](s).`$`
 
 proc sha512_sph(s: string): string =
-  ## ~2.3s/Mhash
+  ## ~1.1s/Mhash
   count[SPH_SHA512](s).`$`
 
+proc sha512_nimcrypto2(s: string): string =
+  ## ~1.1s/Mhash Much slower than sha512_nimcrypto
+  count[NIMCRYPTO_SHA512](s).`$`
+
 proc sha512_nimcrypto(s: string): string =
-  ## ~1.7s/Mhash
+  ## ~0.77s/Mhash
   var ctx: sha512
   init(ctx)
   ctx.update(s)
   ctx.finish().`$`
 
-for fun in [sha512_rhash, sha512_mhash, sha512_sph, sha512_nimcrypto]:
+for fun in [sha512_rhash, sha512_mhash, sha512_sph, sha512_nimcrypto2, sha512_nimcrypto]:
   timeit:
     for i in 1..1000000:
       discard fun($i & "FSEnBSUSusdHf5sd9eS5thkpMVLb_TIUuoNKm1gbq0Y")
